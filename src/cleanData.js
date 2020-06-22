903f55929b21819b371cd653b66032197b8a0929
Max Stuart <maxastuart@gmail.com>
2020-06-22 01:17:19 -0700

    Refactor for clarity, simplicity & eslint

--- src/cleanData.js
+++ src/cleanData.js
@@ -1,35 +1,26 @@
+'use strict';
 
-const newlines = /[\n]+/g
-const spaces = /[\s]+/g
-const leadingPunc = /^[^a-zA-Z0-9]+/
-const trailingPunc = /[^a-zA-Z0-9]+$/
-const spacePeriod = /[\s]+,/g
-const dashes = /\u2013|\u2014/g
+const newlines = /[\n]+/g;
+const spaces = /[\s]+/g;
+const leadingPunc = /^[^a-zA-Z0-9]+/;
+const trailingPunc = /[^a-zA-Z0-9]+$/;
+const spacePeriod = /[\s]+,/g;
+const dashes = /\u2013|\u2014/g;
 
-module.exports = function (site) {
-    for (var key in site) {
-        if (site.hasOwnProperty(key)) {
-            site[key] = cleanField(site[key]);
-        }
-    }
-}
-
-function cleanField(field) {
+const cleanField = (field) => {
     // multiple newlines
-    field = field.replace(newlines, ", ");
+    return field.replace(newlines, ', ')
+        // multiple spaces
+        .replace(spaces, ' ')
+        // space before period
+        .replace(spacePeriod, ',')
+        // leading/trailing punctuation
+        .replace(leadingPunc, '')
+        .replace(trailingPunc, '')
+        // fix dashes
+        .replace(dashes, '-');
+};
 
-    // multiple spaces
-    field = field.replace(spaces, " ");
-
-    // space before period
-    field = field.replace(spacePeriod, ",");
-
-    // leading/trailing punctuation
-    field = field.replace(leadingPunc, "");
-    field = field.replace(trailingPunc, "");
-    
-    // fix dashes
-    field = field.replace(dashes, "-");
-
-    return field;
-}
+module.exports = (site) => {
+    return Object.entries(site).reduce((out, [k, v]) => (v ? (out[k] = cleanField(v), out) : out), {});
+};
